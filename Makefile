657541408b43301fa1018ca79f6af4c5f9aac0e7
Damianos Mylonakis <danmylonakis@gmail.com>
2012-02-17 05:31:24 +0200

    add tests & fix line ending issues

---
+++ Makefile
@@ -0,0 +1,13 @@
+
+
+TESTS = test/*.js
+REPORTER = spec
+
+test:
+	@./node_modules/.bin/mocha \
+		--require should \
+		--reporter $(REPORTER) \
+		--growl \
+		$(TESTS)
+
+.PHONY: test
---
+++ test/parse_full.js
@@ -0,0 +1,23 @@
+var should = require('should'),
+    fs     = require('fs'),
+    ical   = require('../');
+
+describe('Parser', function () {
+  var parser, full_ics_stream
+  var full_ics = fs.readFileSync(__dirname + '/resources/full.ics')
+  beforeEach(function (done) {
+    parser = new ical.Parser()
+    full_ics_stream = fs.createReadStream(__dirname + '/resources/full.ics')
+    done()
+  })
+  describe('parses correctly tests', function () {
+    it('must parse full.ics', function (done) {
+      parser.on('end', done)
+      parser.write(full_ics)
+    })
+    it('must parse full.ics streamed', function (done) {
+      parser.on('end', done);
+      full_ics_stream.pipe(parser)
+    })
+  })
+})
